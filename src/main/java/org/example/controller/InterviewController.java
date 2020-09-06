96b68f39be74b0e2b6cdd2a6919c554e3896ecd1
Sergey <sergeyraychenko@gmail.com>
2020-09-06 14:43:58 +0300

    add InterviewController

---
+++ src/main/java/org/example/controller/InterviewController.java
@@ -0,0 +1,61 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.model.Interview;
+import org.example.model.User;
+import org.example.service.InterviewService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("interviews")
+public class InterviewController {
+
+    private final InterviewService interviewService;
+
+    @PutMapping
+    public ResponseEntity<Interview> save(@RequestBody Interview interview){
+        try {
+            return new ResponseEntity<>(interviewService.save(interview), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Interview> update (@RequestBody Interview interview){
+        try {
+            return new ResponseEntity<>(interviewService.update(interview), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Interview> findById(@PathVariable Integer id){
+        try {
+            return new ResponseEntity<>(interviewService.findById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page> findAll(@RequestParam Integer page, @RequestParam Integer size){
+        return new ResponseEntity<>(interviewService.findAll(page, size), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity delete(@PathVariable Integer id){
+        try {
+            interviewService.delete(interviewService.findById(id));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
